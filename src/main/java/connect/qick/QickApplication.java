467014ea28ad4f1bffcacb078f6732ad59aec443
daehyeong2 <baconbacon1231@gmail.com>
2025-10-17 09:51:24 +0900

    Initialize Commit

---
+++ src/main/java/connect/qick/QickApplication.java
@@ -0,0 +1,13 @@
+package connect.qick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/connect/qick/QickApplicationTests.java
@@ -0,0 +1,13 @@
+package connect.qick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
