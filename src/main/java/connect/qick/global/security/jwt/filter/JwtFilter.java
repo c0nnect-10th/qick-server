3f8d66b948ab666abe673419ff95909997fb0018
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-10-20 20:26:23 +0900

    feat: JwtFilter 생성

---
+++ src/main/java/connect/qick/global/security/jwt/filter/JwtFilter.java
@@ -0,0 +1,32 @@
+package connect.qick.global.security.jwt.filter;
+
+import connect.qick.domain.user.entity.UserEntity;
+import connect.qick.global.security.jwt.JwtExtract;
+import connect.qick.global.security.jwt.JwtProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtExtract jwtExtract;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtExtract.extractTokenFromRequest(request);
+        if (token != null) {
+            SecurityContextHolder.getContext().setAuthentication(jwtExtract.getAuthentication(token));
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
