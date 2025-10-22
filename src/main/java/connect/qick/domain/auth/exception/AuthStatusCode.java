da9fad9c27124ca06aca706e4da1bcaaa555e8f1
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-10-22 17:25:30 +0900

    feat: auth 상태코드 추가 INVALID_T_CODE_EXCEPTION, ALREADY_EXISTS_EXCEPTION

--- src/main/java/connect/qick/domain/auth/exception/AuthStatusCode.java
+++ src/main/java/connect/qick/domain/auth/exception/AuthStatusCode.java
@@ -11,7 +11,10 @@ public enum AuthStatusCode implements StatusCode {
   INVALID_JWT("INVALID_JWT", "유효하지 않은 JWT입니다.", HttpStatus.UNAUTHORIZED),
   INVALID_CREDENTIALS("INVALID_CREDENTIALS", "아이디 또는 비밀번호가 일치하지 않습니다.", HttpStatus.UNAUTHORIZED),
   ACCOUNT_LOCKED("ACCOUNT_LOCKED", "계정이 잠겨 있습니다.", HttpStatus.UNAUTHORIZED),
-  ACCOUNT_DISABLED("ACCOUNT_DISABLED", "계정이 비활성화되었습니다.", HttpStatus.UNAUTHORIZED),;
+  ACCOUNT_DISABLED("ACCOUNT_DISABLED", "계정이 비활성화되었습니다.", HttpStatus.UNAUTHORIZED),
+
+  INVALID_T_CODE_EXCEPTION("INVALID_T_CODE_EXCEPTION", "유효하지 않은 T코드입니다.", HttpStatus.UNAUTHORIZED),
+  ALREADY_EXISTS_EXCEPTION("ALREADY_EXISTS_EXCEPTION", "이미 가입된 사용자입니다.", HttpStatus.CONFLICT),;
 
   private final String code;
   private final String message;
