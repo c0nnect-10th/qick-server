52280306ca359a381c3d4ef6a221c1659f8f8e50
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-12-23 15:12:00 +0900

    feat: swagger 설정

---
+++ src/main/java/connect/qick/global/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package connect.qick.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Qick")
+                        .description("선생님들이 도움이 필요할 때 쉽게 사용할 수 있는 봉사 학생 모집 서비스 Qick입니다.")
+                        .version("v1.0.0")
+                );
+    }
+
+}
