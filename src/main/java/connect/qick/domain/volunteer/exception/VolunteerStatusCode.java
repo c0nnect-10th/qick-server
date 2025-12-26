a972701877476f313a3dc6b7321442c5d57671df
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-12-26 20:59:54 +0900

    feat: 심부름 예외처리

---
+++ src/main/java/connect/qick/domain/volunteer/exception/VolunteerStatusCode.java
@@ -0,0 +1,17 @@
+package connect.qick.domain.volunteer.exception;
+
+import connect.qick.global.exception.status_code.StatusCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum VolunteerStatusCode implements StatusCode {
+  WORK_NOT_FOUND("WORK_NOT_FOUND", "봉사활동이 존재하지 않습니다.", HttpStatus.UNAUTHORIZED),
+  ;
+
+  private final String code;
+  private final String message;
+  private final HttpStatus httpStatus;
+}
