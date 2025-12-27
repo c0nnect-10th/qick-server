2de1ec635f6833e12d124df8bd4fad4552156063
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-12-27 16:05:56 +0900

    refactor: 불필요한 import 삭제

--- src/main/java/connect/qick/global/entity/Base.java
+++ src/main/java/connect/qick/global/entity/Base.java
@@ -7,8 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
-
-import lombok.Builder;
 import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
