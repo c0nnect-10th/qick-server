c10caafed19563924ef304c1a270841d1b3bdc68
최수아 <sua9097@gmail.com>
2026-01-01 20:18:17 +0900

    feat: firebase admin sdk 종속성 추가 및 firebase초기화 설정

---
+++ src/main/java/connect/qick/global/config/FirebaseConfig.java
@@ -0,0 +1,35 @@
+package connect.qick.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Value("${firebase.service-account-key}")
+    private String serviceAccountKeyPath;
+
+    @PostConstruct
+    public void init() throws IOException {
+        try (InputStream serviceAccount = new ClassPathResource(serviceAccountKeyPath).getInputStream()) {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (IOException e) {
+            System.err.println("Error initializing Firebase: " + e.getMessage());
+            throw new IOException("Failed to initialize Firebase with provided service account key.", e);
+        }
+    }
+}
